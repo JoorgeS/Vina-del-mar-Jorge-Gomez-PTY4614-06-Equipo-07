68b091e2890add7e04b75dfeb44bd41091f1c97b
Jorge Sepulveda <102179260+JoorgeS@users.noreply.github.com>
2025-10-12 21:07:58 -0300

    Delete Fase 2/Evidencias Proyecto/corevota directory

--- Fase 2/Evidencias Proyecto/corevota/hash_generador.php
+++
@@ -1,9 +0,0 @@
-<?php
-// La contraseña de prueba que quieres usar
-$password = '123'; 
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-echo "Contraseña de prueba: " . $password . "<br>";
-echo "HASH generado: <b>" . $hashed_password . "</b><br><br>";
-echo "Copia el texto del HASH (comienza con \$2y\$...)";
-?>
